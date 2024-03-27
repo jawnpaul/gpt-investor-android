51a5fdbb89061361528ba127c5ac7c7730bfc18c
jawnpaul <jpaul5246@gmail.com>
2024-03-27 22:18:37 +0100

    added top app bar

--- app/src/main/java/com/example/gptinvestor/features/investor/presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/example/gptinvestor/features/investor/presentation/ui/HomeScreen.kt
@@ -2,6 +2,16 @@ package com.example.gptinvestor.features.investor.presentation.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -9,11 +19,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavHostController
+import com.example.gptinvestor.R
 import com.example.gptinvestor.features.investor.presentation.viewmodel.HomeViewModel
 import timber.log.Timber
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier, navController: NavHostController, viewModel: HomeViewModel) {
     var showLandingScreen by remember { mutableStateOf(true) }
@@ -21,18 +35,46 @@ fun HomeScreen(modifier: Modifier, navController: NavHostController, viewModel: 
         LandingScreen(onTimeout = { showLandingScreen = false })
     } else {
         // Home Screen
-        Box(modifier = Modifier.padding(horizontal = 8.dp)) {
-            val state = viewModel.allSector.collectAsState()
-            // row of sectors
-            SectorChoiceQuestion(
-                possibleAnswers = state.value.sectors,
-                selectedAnswer = state.value.selected,
-                onOptionSelected = {
-                    Timber.e("Selected")
-                    viewModel.selectSector(it)
-                }
-            )
-            // list of companies
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.app_name),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    actions = {
+                        IconButton(onClick = {
+
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            }
+        ) { innerPadding ->
+
+            Box(modifier = Modifier.padding(innerPadding)) {
+                val state = viewModel.allSector.collectAsState()
+                // row of sectors
+                SectorChoiceQuestion(
+                    possibleAnswers = state.value.sectors,
+                    selectedAnswer = state.value.selected,
+                    onOptionSelected = {
+                        viewModel.selectSector(it)
+                    }
+                )
+                // list of companies
+            }
         }
     }
 }

9654ca759fca01115278ab2ae27bd53cadcf9a2d
jawnpaul <jpaul5246@gmail.com>
2024-04-06 17:52:06 +0100

    implemented rich text formatting for financials

--- app/src/main/java/com/example/gptinvestor/features/company/presentation/ui/CompanyDataScreen.kt
+++ app/src/main/java/com/example/gptinvestor/features/company/presentation/ui/CompanyDataScreen.kt
@@ -3,8 +3,10 @@ package com.example.gptinvestor.features.company.presentation.ui
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -20,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +30,8 @@ import androidx.compose.ui.unit.dp
 import com.example.gptinvestor.R
 import com.example.gptinvestor.features.company.presentation.model.CompanyFinancialsPresentation
 import com.example.gptinvestor.features.company.presentation.viewmodel.CompanyViewModel
+import com.halilibo.richtext.ui.Table
+import com.halilibo.richtext.ui.material3.RichText
 
 @Composable
 fun CompanyDataScreen(modifier: Modifier, viewModel: CompanyViewModel) {
@@ -113,79 +118,141 @@ private fun CardNumbers(title: String, financials: CompanyFinancialsPresentation
                 .weight(1f)
                 .padding(horizontal = 8.dp)
         ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(top = 8.dp)
-            )
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+                IconButton(onClick = { expanded = !expanded }, modifier = Modifier.align(Alignment.TopEnd)) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                        contentDescription = if (expanded) {
+                            stringResource(R.string.show_less)
+                        } else {
+                            stringResource(R.string.show_more)
+                        }
+                    )
+                }
+            }
             if (expanded) {
                 if (financials != null) {
-                    Column {
-                        Row(modifier = Modifier.padding(top = 8.dp)) {
-                            Text(
-                                text = stringResource(id = R.string.open),
-                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            Text(text = financials.open)
-                        }
+                    RichText {
+                        Table(modifier = Modifier.fillMaxWidth()) {
+                            row {
+                                cell {
+                                    Text(
+                                        text = stringResource(id = R.string.open),
+                                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                                cell { Text(financials.open) }
+                            }
 
-                        Row(modifier = Modifier.padding(top = 8.dp)) {
-                            Text(
-                                text = stringResource(id = R.string.high),
-                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            Text(text = financials.high)
-                        }
+                            row {
+                                cell {
+                                    Text(
+                                        text = stringResource(id = R.string.high),
+                                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                                cell { Text(financials.high) }
+                            }
 
-                        Row(modifier = Modifier.padding(top = 8.dp)) {
-                            Text(
-                                text = stringResource(id = R.string.low),
-                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            Text(text = financials.low)
-                        }
+                            row {
+                                cell {
+                                    Text(
+                                        text = stringResource(id = R.string.low),
+                                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                                cell { Text(financials.low) }
+                            }
 
-                        Row(modifier = Modifier.padding(top = 8.dp)) {
-                            Text(
-                                text = stringResource(id = R.string.close),
-                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            Text(text = financials.close)
+                            row {
+                                cell {
+                                    Text(
+                                        text = stringResource(id = R.string.close),
+                                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                                cell { Text(financials.close) }
+                            }
+                            row {
+                                cell {
+                                    Text(
+                                        text = stringResource(id = R.string.volume),
+                                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                                cell { Text(financials.volume) }
+                            }
+                            row {
+                                cell {
+                                    Text(
+                                        text = stringResource(id = R.string.market_cap),
+                                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                                    )
+                                }
+                                cell { Text(financials.marketCap) }
+                            }
                         }
-                        Row(modifier = Modifier.padding(top = 8.dp)) {
-                            Text(
-                                text = stringResource(id = R.string.volume),
-                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            Text(text = financials.volume)
-                        }
-                        Row(modifier = Modifier.padding(top = 8.dp)) {
-                            Text(
-                                text = stringResource(id = R.string.market_cap),
-                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(end = 8.dp)
-                            )
-                            Text(text = financials.marketCap)
-                        }
+                        /*Column {
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Text(
+                                    text = stringResource(id = R.string.open),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(text = financials.open)
+                            }
+
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Text(
+                                    text = stringResource(id = R.string.high),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(text = financials.high)
+                            }
+
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Text(
+                                    text = stringResource(id = R.string.low),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(text = financials.low)
+                            }
+
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Text(
+                                    text = stringResource(id = R.string.close),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(text = financials.close)
+                            }
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Text(
+                                    text = stringResource(id = R.string.volume),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(text = financials.volume)
+                            }
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Text(
+                                    text = stringResource(id = R.string.market_cap),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                                    modifier = Modifier.padding(end = 8.dp)
+                                )
+                                Text(text = financials.marketCap)
+                            }
+                        }*/
                     }
                 }
             }
-        }
-
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
-            )
         }
     }
 }

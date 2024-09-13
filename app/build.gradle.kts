3e7b765ebfb760581291d9533ae7a5220fc468ec
jawnpaul <jpaul5246@gmail.com>
2024-09-13 14:43:36 +0100

    worked on the splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,6 +139,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.dagger.hilt)
     implementation(libs.timber)
     implementation(libs.androidx.navigation.compose)
--- app/src/main/java/com/thejawnpaul/gptinvestor/ui/theme/Theme.kt
+++ app/src/main/java/com/thejawnpaul/gptinvestor/ui/theme/Theme.kt
@@ -1,19 +1,16 @@
 package com.thejawnpaul.gptinvestor.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -101,6 +98,13 @@ private val LightColorScheme = lightColorScheme(
      */
 )
 
+val BottomSheetShape = RoundedCornerShape(
+    topStart = 0.dp,
+    topEnd = 0.dp,
+    bottomStart = 20.dp,
+    bottomEnd = 20.dp
+)
+
 @Composable
 fun GPTInvestorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -117,18 +121,18 @@ fun GPTInvestorTheme(
         darkTheme -> DarkColors
         else -> LightColors
     }
-    val view = LocalView.current
+    /*val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
-    }
+    }*/
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }

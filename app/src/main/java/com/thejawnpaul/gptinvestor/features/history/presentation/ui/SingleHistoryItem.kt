878167beb31e05a147fd1a9feb0cd44abd02c6d2
jawnpaul <jpaul5246@gmail.com>
2024-10-11 17:15:27 +0100

    improved SingleHistoryItem.kt

--- app/src/main/java/com/thejawnpaul/gptinvestor/features/history/presentation/ui/SingleHistoryItem.kt
+++ app/src/main/java/com/thejawnpaul/gptinvestor/features/history/presentation/ui/SingleHistoryItem.kt
@@ -5,18 +5,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thejawnpaul.gptinvestor.R
 import com.thejawnpaul.gptinvestor.features.conversation.domain.model.StructuredConversation
+import com.thejawnpaul.gptinvestor.ui.theme.GPTInvestorTheme
 
 @Composable
 fun SingleHistoryItem(
@@ -24,12 +28,12 @@ fun SingleHistoryItem(
     conversation: StructuredConversation,
     onClick: (id: Long) -> Unit
 ) {
-    Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+    Column(modifier = Modifier.padding(horizontal = 0.dp)) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .clickable { onClick(conversation.id) }
-                .padding(vertical = 8.dp),
+                .padding(vertical = 16.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
         ) {
@@ -48,5 +52,32 @@ fun SingleHistoryItem(
         }
 
         HorizontalDivider()
+    }
+}
+
+@Preview
+@Composable
+fun ItemPreview(modifier: Modifier = Modifier) {
+    GPTInvestorTheme {
+        Surface {
+            Column(modifier = Modifier.fillMaxSize()) {
+
+                SingleHistoryItem(
+                    modifier = Modifier,
+                    conversation = StructuredConversation(
+                        id = 0,
+                        title = "What factors are influencing Netflix stock price today?"
+                    )
+                ) { }
+
+                SingleHistoryItem(
+                    modifier = Modifier,
+                    conversation = StructuredConversation(
+                        id = 0,
+                        title = "What factors are influencing Netflix stock price today?"
+                    )
+                ) { }
+            }
+        }
     }
 }

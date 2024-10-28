8a487a82f686c786cac4dc1a4f6c658ea683eded
jawnpaul <jpaul5246@gmail.com>
2024-10-28 11:23:28 +0100

    Removed unnecessary surface from sector

--- app/src/main/java/com/thejawnpaul/gptinvestor/features/investor/presentation/ui/SectorChoiceInput.kt
+++ app/src/main/java/com/thejawnpaul/gptinvestor/features/investor/presentation/ui/SectorChoiceInput.kt
@@ -1,15 +1,11 @@
 package com.thejawnpaul.gptinvestor.features.investor.presentation.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,9 +14,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thejawnpaul.gptinvestor.features.company.domain.model.SectorInput
@@ -58,35 +51,26 @@ fun SingleSectorChoice(
     onOptionSelected: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Surface(
-        color = Color.Transparent,
-        modifier = modifier
-            .clip(MaterialTheme.shapes.small)
-            .selectable(
-                selected,
+    when (input) {
+        is SectorInput.AllSector -> {
+            // Text(text = "All")
+            FilterChip(
+                modifier = Modifier,
+                selected = selected,
                 onClick = onOptionSelected,
-                role = Role.RadioButton
-            )
+                label = { Text(text = "All") })
+        }
 
-    ) {
-        when (input) {
-            is SectorInput.AllSector -> {
-                // Text(text = "All")
-                FilterChip(
-                    selected = selected,
-                    onClick = onOptionSelected,
-                    label = { Text(text = "All") })
-            }
-
-            is SectorInput.CustomSector -> {
-                // Text(text = input.sectorName)
-                FilterChip(
-                    selected = selected,
-                    onClick = onOptionSelected,
-                    label = { Text(text = input.sectorName) })
-            }
+        is SectorInput.CustomSector -> {
+            // Text(text = input.sectorName)
+            FilterChip(
+                modifier = Modifier,
+                selected = selected,
+                onClick = onOptionSelected,
+                label = { Text(text = input.sectorName) })
         }
     }
+
 }
 
 @Preview

082bc1749f3d7bf0e621e36dbe820ac107c888a9
John Paul <jpaul5246@gmail.com>
2025-05-21 11:50:17 +0100

    Refactor: Enhance SearchBar with Custom Styling and Functionality
    - Replaced the Material 3 `SearchBar` with a custom implementation using `Surface` and `BasicTextField` for greater control over appearance and behavior.
    - Applied a `RoundedCornerShape` and `BorderStroke` to the `SearchBarCustom` for a distinct visual style.
    - Integrated a custom search icon (`ic_search.xml`) within the search bar.
    - Implemented placeholder text visibility based on whether the query is blank.
    - Customized text styles and cursor color to align with the app's theme.
    - Added `ic_search.xml` drawable resource.

--- app/src/main/java/com/thejawnpaul/gptinvestor/features/discover/SearchBar.kt
+++ app/src/main/java/com/thejawnpaul/gptinvestor/features/discover/SearchBar.kt
@@ -1,37 +1,90 @@
 package com.thejawnpaul.gptinvestor.features.discover
 
-import androidx.compose.material.icons.Icons
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.thejawnpaul.gptinvestor.R
 import com.thejawnpaul.gptinvestor.theme.GPTInvestorTheme
+import com.thejawnpaul.gptinvestor.theme.LocalGPTInvestorColors
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchBarCustom(modifier: Modifier = Modifier, query: String, placeHolder: String, onQueryChange: (newQuery: String) -> Unit, onSearch: (query: String) -> Unit) {
-    SearchBar(
+fun SearchBarCustom(modifier: Modifier, query: String, placeHolder: String, onQueryChange: (newQuery: String) -> Unit, onSearch: (query: String) -> Unit) {
+    val gptInvestorColors = LocalGPTInvestorColors.current
+
+    Surface(
         modifier = modifier,
-        inputField = {
-            SearchBarDefaults.InputField(
-                query = query,
-                onQueryChange = onQueryChange,
-                onSearch = onSearch,
-                expanded = false,
-                onExpandedChange = { },
-                placeholder = { Text(placeHolder, maxLines = 1, overflow = TextOverflow.Clip) },
-                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) }
+        shape = RoundedCornerShape(corner = CornerSize(20.dp)),
+        border = BorderStroke(
+            2.dp,
+            MaterialTheme.colorScheme.outlineVariant
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(painter = painterResource(R.drawable.ic_search), contentDescription = null)
+
+            BasicTextField(
+                modifier = Modifier.weight(1F),
+                value = query,
+                onValueChange = onQueryChange,
+                maxLines = 1,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        onSearch(query)
+                    }
+                ),
+                decorationBox = { innerTextField ->
+                    Box {
+                        if (query.trim().isBlank()) {
+                            Text(
+                                text = placeHolder,
+                                style = MaterialTheme.typography.titleSmall,
+                                color = gptInvestorColors.textColors.secondary50
+                            )
+                        }
+                        innerTextField()
+                    }
+                },
+                textStyle = MaterialTheme.typography.bodyMedium.copy(
+                    color = MaterialTheme.colorScheme.onSurface
+                ),
+                cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface)
             )
-        },
-        expanded = false,
-        onExpandedChange = { }
-    ) {}
+        }
+    }
 }
 
 @Preview
@@ -39,9 +92,10 @@ fun SearchBarCustom(modifier: Modifier = Modifier, query: String, placeHolder: S
 fun SearchPreview(modifier: Modifier = Modifier) {
     GPTInvestorTheme {
         SearchBarCustom(
+            modifier = Modifier,
             query = "",
             onQueryChange = {},
-            placeHolder = "Search companies",
+            placeHolder = "Search any company",
             onSearch = {}
         )
     }

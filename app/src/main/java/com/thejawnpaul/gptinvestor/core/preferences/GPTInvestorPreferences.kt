9a81182137d1382ee94ac5a30da1226a7c6280a3
John Paul <jpaul5246@gmail.com>
2025-05-28 20:03:27 +0100

    feat: Add preferences for query usage tracking
    - Added `QUERY_LAST_DATE_KEY` to store the last date a query was made.
    - Added `QUERY_USAGE_COUNT_KEY` to store the number of queries made on a given day.
    - Implemented `queryLastDate` flow to observe the last query date.
    - Implemented `setQueryLastDate` and `clearQueryLastDate` to manage the last query date.
    - Implemented `queryUsageCount` flow to observe the query usage count.
    - Implemented `setQueryUsageCount` to update the query usage count.

--- app/src/main/java/com/thejawnpaul/gptinvestor/core/preferences/GPTInvestorPreferences.kt
+++ app/src/main/java/com/thejawnpaul/gptinvestor/core/preferences/GPTInvestorPreferences.kt
@@ -2,6 +2,7 @@ package com.thejawnpaul.gptinvestor.core.preferences
 
 import android.content.Context
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -17,6 +18,8 @@ class GPTInvestorPreferences @Inject constructor(@ApplicationContext private val
 
     companion object {
         private val THEME_KEY = stringPreferencesKey("theme_preference")
+        private val QUERY_LAST_DATE_KEY = stringPreferencesKey("query_last_day_preference")
+        private val QUERY_USAGE_COUNT_KEY = intPreferencesKey("query_usage_count_preference")
         private val Context.dataStore by preferencesDataStore("gpt_investor_preferences")
     }
 
@@ -33,6 +36,32 @@ class GPTInvestorPreferences @Inject constructor(@ApplicationContext private val
     suspend fun clearThemePreference() {
         dataStore.edit { preferences ->
             preferences.remove(THEME_KEY)
+        }
+    }
+
+    val queryLastDate: Flow<String?> = dataStore.data.map { preferences ->
+        preferences[QUERY_LAST_DATE_KEY]
+    }
+
+    suspend fun setQueryLastDate(date: String) {
+        dataStore.edit { preferences ->
+            preferences[QUERY_LAST_DATE_KEY] = date
+        }
+    }
+
+    suspend fun clearQueryLastDate() {
+        dataStore.edit { preferences ->
+            preferences.remove(QUERY_LAST_DATE_KEY)
+        }
+    }
+
+    val queryUsageCount: Flow<Int?> = dataStore.data.map { preferences ->
+        preferences[QUERY_USAGE_COUNT_KEY]
+    }
+
+    suspend fun setQueryUsageCount(count: Int) {
+        dataStore.edit { preferences ->
+            preferences[QUERY_USAGE_COUNT_KEY] = count
         }
     }
 }

cb56fc004755de3f08b87b6506f9c7ce5781ccd1
John Paul <jpaul5246@gmail.com>
2025-09-19 08:49:16 -0400

    Release: Update app version to 1.1.4
    - Incremented `versionCode` to 13.
    - Updated `versionName` to "1.1.4".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.thejawnpaul.gptinvestor"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.1.3"
+        versionCode = 13
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

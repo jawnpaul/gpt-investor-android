66053334ce7b313bfa62a9de897110ac46e392ad
jawnpaul <jpaul5246@gmail.com>
2026-05-18 23:41:05 -0400

    Refactor Koin initialization to Application class
    - **Dependency Injection**
        - Move Koin initialization from `MainActivity` to `GPTInvestorApplication` using `startKoin`.
        - Configure `androidContext` within the `koinConfiguration` block in the application's `onCreate` method.
        - Remove the `KoinApplication` composable wrapper from `MainActivity`, simplifying the entry point and ensuring dependency injection is set up before the UI starts.

--- androidApp/src/main/kotlin/com/thejawnpaul/gptinvestor/GPTInvestorApplication.kt
+++ androidApp/src/main/kotlin/com/thejawnpaul/gptinvestor/GPTInvestorApplication.kt
@@ -7,11 +7,21 @@ import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.appcheck.appCheck
 import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.thejawnpaul.gptinvestor.core.di.GPTKoinApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.plugin.module.dsl.koinConfiguration
 
 class GPTInvestorApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin(
+            koinConfiguration<GPTKoinApp> {
+                androidContext(this@GPTInvestorApplication)
+            }
+        )
 
         FirebaseApp.initializeApp(this)
 
